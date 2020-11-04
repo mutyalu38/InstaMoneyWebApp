57cb8b86d17b7e585a9651c952c92e6e391c48be
mutyalu38 <indukurimutyalu2020@gmail.com>
2020-11-04 09:03:44 +0530

    add bankmitra and loansCustomerOccupatioDTO class addedd

---
+++ src/main/java/com/gtids/InstaMoney/model/Bankmitra.java
@@ -0,0 +1,128 @@
+package com.gtids.InstaMoney.model;
+
+public class Bankmitra {
+	private Integer num;
+	private Integer sno;
+	private String district;
+	private String ro;
+	private String area_manager_id;
+	private String area_manager;
+	private String branch;
+	private String village;
+	private String id;
+	private String terminal_id;
+	private String name;
+	private String contact_no;
+	private String aadhaar_number;
+	private String androidid;
+	private String email;
+	private Integer role;
+	private Integer status;
+	public Integer getNum() {
+		return num;
+	}
+	public void setNum(Integer num) {
+		this.num = num;
+	}
+	public Integer getSno() {
+		return sno;
+	}
+	public void setSno(Integer sno) {
+		this.sno = sno;
+	}
+	public String getDistrict() {
+		return district;
+	}
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+	public String getRo() {
+		return ro;
+	}
+	public void setRo(String ro) {
+		this.ro = ro;
+	}
+	public String getArea_manager_id() {
+		return area_manager_id;
+	}
+	public void setArea_manager_id(String area_manager_id) {
+		this.area_manager_id = area_manager_id;
+	}
+	public String getArea_manager() {
+		return area_manager;
+	}
+	public void setArea_manager(String area_manager) {
+		this.area_manager = area_manager;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public String getVillage() {
+		return village;
+	}
+	public void setVillage(String village) {
+		this.village = village;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTerminal_id() {
+		return terminal_id;
+	}
+	public void setTerminal_id(String terminal_id) {
+		this.terminal_id = terminal_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContact_no() {
+		return contact_no;
+	}
+	public void setContact_no(String contact_no) {
+		this.contact_no = contact_no;
+	}
+	public String getAadhaar_number() {
+		return aadhaar_number;
+	}
+	public void setAadhaar_number(String aadhaar_number) {
+		this.aadhaar_number = aadhaar_number;
+	}
+	public String getAndroidid() {
+		return androidid;
+	}
+	public void setAndroidid(String androidid) {
+		this.androidid = androidid;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Integer getRole() {
+		return role;
+	}
+	public void setRole(Integer role) {
+		this.role = role;
+	}
+	public Integer getStatus() {
+		return status;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/gtids/InstaMoney/model/LoansCustomerOccupationDTO.java
@@ -0,0 +1,8 @@
+package com.gtids.InstaMoney.model;
+
+public interface LoansCustomerOccupationDTO {
+	String getBeneficiary_lineofactivity();
+
+	int getCountOfBeneficiary_lineofactivity();
+
+}
